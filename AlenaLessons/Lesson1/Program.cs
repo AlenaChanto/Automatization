391e71145350842c34b01b742e448dee43c3ab99
AlenaChanto <alena.chanto@arcadia.spb.ru>
2022-02-24 19:09:12 +0300

    Tasks are added

---
+++ AlenaLessons/Lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson1_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 2;
+            double y = 3;
+            double z = 4;
+            double result1 = 0;
+            double result2 = 0;
+            double result3 = 0;
+
+            result1 = (x + y) * (Math.Pow(z, 2) + 1);
+            result2 = (x / z - 1) * Math.Sqrt(y);
+            result3 = (x * y + y * z) / Math.Pow(z, 3);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlenaLessons/Lesson1_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson1_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = 5;
+            double length = 3;
+            double height = 4;
+            double diagonal = 0;
+
+            diagonal = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(length, 2) + Math.Pow(height, 2));
+
+            Console.WriteLine(diagonal);
+            Console.ReadKey();
+        }
+    }
+}
