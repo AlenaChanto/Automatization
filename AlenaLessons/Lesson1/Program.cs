69bb0760970a6292b91fb84cbd6c33d4b78e449b
AlenaChanto <alena.chanto@arcadia.spb.ru>
2022-03-03 18:16:56 +0300

    Tasks added

--- AlenaLessons/Lesson1/Program.cs
+++ AlenaLessons/Lesson1/Program.cs
@@ -1,48 +1,90 @@
 ﻿using System;
 
-namespace Lesson1_Task3
+namespace Lesson1_Task1
 {
     internal class Program
     {
         static void Main(string[] args)
-       {
-         Console.WriteLine("Enter x value: ");
-            double x = Convert.ToDouble(Console.ReadLine());
+        {
+            double x = myMethod("x");
+            double y = myMethod("y");
+            double z = myMethod("z");
 
-            Console.WriteLine("Enter y value: ");
-            double y = Convert.ToDouble(Console.ReadLine());
+            double result1 = 0;
+            double result2 = 0;
+            double result3 = 0;
 
-           
+            result1 = (x + y) * (Math.Pow(z, 2) + 1);
+            result2 = (x / z - 1) * Math.Sqrt(y);
+            result3 = (x * y + y * z) / Math.Pow(z, 3);
 
-            Console.WriteLine("Enter z value: ");
-            double z = 0;
-            bool notNumber = false;
+            Console.WriteLine($"First result: {result1}");
+            Console.WriteLine($"Second result: {result2}");
+            Console.WriteLine($"Third result: {result3}");
 
-            notNumber = Double.TryParse(Console.ReadLine(), out z);
-            if (!notNumber)
+            /* Console.WriteLine("Enter y value: ");
+             double y = Convert.ToDouble(Console.ReadLine());
+
+             Console.WriteLine("Enter z value: ");
+
+             while (true)
+             {
+                 double z = 0;
+                 bool successful = false; 
+
+                 successful = Double.TryParse(Console.ReadLine(), out z); // if successful is number (true), give z a value
+                 if (!successful) // if its not a number (not successful)
+                 {
+                     Console.WriteLine("Enter the number! ");
+                 }
+                 else if (z == 0)
+                 {
+                     Console.WriteLine("The value Z can not be null! Enter another number ");
+                 }
+                 else
+                 {
+                     double result1 = 0;
+                     double result2 = 0;
+                     double result3 = 0;
+
+                     result1 = (x + y) * (Math.Pow(z, 2) + 1);
+                     result2 = (x / z - 1) * Math.Sqrt(y);
+                     result3 = (x * y + y * z) / Math.Pow(z, 3);
+
+                     Console.WriteLine($"First result: {result1}");
+                     Console.WriteLine($"Second result: {result2}");
+                     Console.WriteLine($"Third result: {result3}");
+                 }
+
+             } */
+
+
+        }
+        public static Double myMethod(String letter)
+        {
+            Console.WriteLine($"Enter {letter} value: ");
+            double letterValue = 0;
+            while (true)
             {
-                Console.WriteLine("Enter the number! ");
-            }
-            else if (z == 0)
-            {
-                Console.WriteLine("The value Z can not be null! ");
-            }
-            else
-            {
-                double result1 = 0;
-                double result2 = 0;
-                double result3 = 0;
 
-                result1 = (x + y) * (Math.Pow(z, 2) + 1);
-                result2 = (x / z - 1) * Math.Sqrt(y);
-                result3 = (x * y + y * z) / Math.Pow(z, 3);
+                bool successful = false;
 
-                Console.WriteLine($"First result: {result1}");
-                Console.WriteLine($"Second result: {result2}");
-                Console.WriteLine($"Third result: {result3}");
+                successful = Double.TryParse(Console.ReadLine(), out letterValue); // if successful is number (true), give x a value
+                if (!successful) // if its not a number (not successful)
+                {
+                    Console.WriteLine("Enter the number! ");
+                }
+                else if (letterValue == 0)
+                {
+                    Console.WriteLine($"The value {letter} can not be null! Enter another number ");
+                }
+                else
+                {
+                    break;
+                }
+
             }
-
-           
+            return letterValue;
         }
     }
 }
