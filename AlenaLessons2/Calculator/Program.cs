6c32863f04962e6398b64b3dca8a786b8ca4c77f
AlenaChanto <alena.chanto@arcadia.spb.ru>
2022-03-17 16:08:59 +0300

    Task3 is added.

---
+++ AlenaLessons2/Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See husing System;
+
+namespace Lesson1_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first digit");
+            string enteredLogin = Console.ReadLine();
+
+
+
+
+
+        }
+    }
+
+    // public static double Authentification(string data)
+}
