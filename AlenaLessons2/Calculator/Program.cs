ead7b20e415d4a374e739665d3e6dddcbd299e9e
AlenaChanto <alena.chanto@arcadia.spb.ru>
2022-03-17 16:09:34 +0300

    Task3 is added.

--- AlenaLessons2/Calculator/Program.cs
+++ AlenaLessons2/Calculator/Program.cs
@@ -6,15 +6,79 @@ namespace Lesson1_Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first digit");
-            string enteredLogin = Console.ReadLine();
+            double digit1 = myMethod("first digit"); //вызов метода, предлагающего по очереди ввести две цифры
+            double digit2 = myMethod("second digit");
 
+            Console.WriteLine("Enter the math operation +, -, /, *, ^"); //ввод одного из допустимых символов
+            char[] arraySymbols = new char[] { '+', '-', '/', '*', '^' }; //объявление массива с разрешенными символами
 
+            while (true)
+            {
+                char enteredsymbol = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                bool successful = false;
+                successful = Enumerable.Contains(arraySymbols, enteredsymbol); //в переменную кладем сравнение элемента массива с введенным символом.
 
+                if (!successful) //если введенный символ не соответствует ни одному элементу массива, то выводим ошибку
+                {
+                    Console.WriteLine("Enter the suggested symbol! ");
+                }
+                else if (enteredsymbol.Equals(arraySymbols[0])) //если соответствует первому символу из массива, тогда выполняем сложение, и тд.
+                {
+                    Console.WriteLine();
+                    double sum = digit1 + digit2;
+                    Console.WriteLine($"Result is: {sum}");
+                }
+                else if (enteredsymbol.Equals(arraySymbols[1]))
+                {
+                    Console.WriteLine();
+                    double subtraction = digit1 - digit2;
+                    Console.WriteLine($"Result is: {subtraction}");
+                }
+                else if (enteredsymbol.Equals(arraySymbols[2]))
+                {
+                    Console.WriteLine();
+                    double devision = digit1 / digit2;
+                    Console.WriteLine($"Result is: {devision}");
+                }
+                else if (enteredsymbol.Equals(arraySymbols[3]))
+                {
+                    Console.WriteLine();
+                    double multiplication = digit1 * digit2;
+                    Console.WriteLine($"Result is: {multiplication}");
+                }
+                else if (enteredsymbol.Equals(arraySymbols[4]))
+                {
+                    Console.WriteLine();
+                    double exponent = Math.Pow(digit1, digit2);
+                    Console.WriteLine($"Result is: {exponent}");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
 
+        public static Double myMethod(String digit)
+        {
+            Console.WriteLine($"Enter {digit} value: ");
+            double enteredDigit = 0;
+            while (true)
+            {
+                bool successful = false;
 
+                successful = Double.TryParse(Console.ReadLine(), out enteredDigit); // if successful is number (true), give first entered digit a value
+                if (!successful) // if its not a number (not successful)
+                {
+                    Console.WriteLine("Enter the digit! ");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return enteredDigit;
         }
     }
-
-    // public static double Authentification(string data)
 }
